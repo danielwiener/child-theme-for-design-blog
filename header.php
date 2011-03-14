303a027b7bf98db753ffd8e8f231cc1c8f7dab49
danielwiener <daniel@danielwiener.com>
2011-03-13 21:57:57 -0400

    redoing things after deleting plugins whose resources were being used even though they were not activated.

--- header.php
+++ header.php
@@ -82,7 +82,7 @@
 				<div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentyten' ); ?>"><?php _e( 'Skip to content', 'twentyten' ); ?></a></div>
 				<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu.  The menu assiged to the primary position is the one used.  If none is assigned, the menu with the lowest ID is used.  */ ?>
 				<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
-				<a href="http://twitter.com/luraydesign" target="_blank" title="Follow me"><img src="/wp-content/plugins/fixed-social-buttons/img/twit2.png" border="0" width="20" height="20" align="right" /></a><a href="http://www.facebook.com/pages/Luraydesign/141326115929696" target="_blank"  title="Facebook profile"><img src="/wp-content/plugins/fixed-social-buttons/img/face2.png" border="0" width="20" height="20" align="right" /></a>
+				<a href="http://twitter.com/luraydesign" target="_blank" title="Follow me"><img src="/wp-content/themes/luraydesign/images/twit2.png" border="0" width="20" height="20" align="right" /></a><a href="http://www.facebook.com/pages/Luraydesign/141326115929696" target="_blank"  title="Facebook profile"><img src="/wp-content/themes/luraydesign/images/face2.png" border="0" width="20" height="20" align="right" /></a>
 			</div><!-- #access -->
 		</div><!-- #masthead -->
 	</div><!-- #header -->
