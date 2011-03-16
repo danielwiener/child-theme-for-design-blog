c6b3a5f69236c383a50f1d41b129ac54cb7315a2
danielwiener <daniel@danielwiener.com>
2011-03-16 13:45:26 -0400

    slide show goes slower

--- js/luraydesign.js
+++ js/luraydesign.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function($) { 
 	    $('.slideshow').show();
 	    $('.slideshow').cycle({
-			fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+			fx: 'fade', // choose your transition type, ex: fade, scrollUp, shuffle, etc... 
+			speed: 2500,
+			timeout: 8000
 		});
 	});
